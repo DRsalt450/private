f99e244f620d9676bc9a2a3db04d623e351a62cf
DRsalt450 <grivalsky.simon@norskakladno.cz>
2025-02-27 16:28:56 +0100

    choice

---
+++ choice/program.py
@@ -0,0 +1,18 @@
+import random
+x = random.choice
+
+
+
+
+
+
+import random
+
+znameni = [
+    "Beran", "Býk", "Blíženci", "Rak", "Lev", "Panna",
+    "Váhy", "Štír", "Střelec", "Kozoroh", "Vodnář", "Ryby"
+]
+
+nahodne_znameni = random.choice(znameni)
+
+print("Náhodné znamení zvěrokruhu:")
